703826b157b4aa579b9b5f30433235f0cd06ca5b
blam <benlam2826@gmail.com>
2025-10-17 22:48:44 -0400

    a3 JavaScript

--- app/(Kambaz)/Courses/[cid]/layout.tsx
+++ app/(Kambaz)/Courses/[cid]/layout.tsx
@@ -1,28 +1,25 @@
 import { ReactNode } from "react";
-import CourseNavigation from "./Navigation";
 import { FaAlignJustify } from "react-icons/fa6";
+import CourseNavigation from "./Navigation";
 import { courses } from "../../Database";
-import Breadcrumb from "./Breadcrumb";
 
-export default async function CoursesLayout({
+export default function CoursesLayout({
   children,
   params,
 }: {
   children: ReactNode;
   params: { cid: string };
 }) {
-  const { cid } = await params;
+  const { cid } = params;
   const course = courses.find((c) => c._id === cid);
 
   return (
     <div id="wd-courses">
       <h2 className="text-danger">
         <FaAlignJustify className="me-4 fs-4 mb-1" />
-        {course?.name ?? `Course ${cid}`}
+        {course ? course.name : `Course ${cid}`}
       </h2>
-      <Breadcrumb course={course} />
       <hr />
-
       <div className="d-flex">
         <div className="d-none d-md-block">
           <CourseNavigation />
