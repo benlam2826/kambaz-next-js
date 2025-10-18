04ca9f0bf3977ad4f097192471c20466c009cfaf
blam <benlam2826@gmail.com>
2025-10-17 22:46:25 -0400

    a3 JavaScript

---
+++ app/(Kambaz)/Courses/[cid]/Grades/page.tsx
@@ -0,0 +1,8 @@
+export default function GradesPage() {
+    return (
+        <div id="wd-grades" className="container pt-3">
+            <h3>Grades</h3>
+            <p className="text-secondary">No grades yet.</p>
+        </div>
+    );
+}
---
+++ app/(Kambaz)/Courses/[cid]/Quizzes/page.tsx
@@ -0,0 +1,8 @@
+export default function QuizzesPage() {
+    return (
+        <div id="wd-quizzes" className="container pt-3">
+            <h3>Quizzes</h3>
+            <p className="text-secondary">No quizzes yet.</p>
+        </div>
+    );
+}
