a08d44008efb35a9d94100cb548840ab3e4af70b
blam <benlam2826@gmail.com>
2025-11-18 22:21:05 -0500

    a5

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -20,6 +20,13 @@ const eslintConfig = [
       "next-env.d.ts",
     ],
   },
+
+  // Our overrides
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
